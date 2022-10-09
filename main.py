6bddd711d92f7768c4bd2dccd6378a3544dbfcb3
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2022-10-09 23:09:06 +0300

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def solution():
-    pass
+from sys import argv
+import network as net
 
-#Starting
+
+class Classify:
+    IP_parameter = str(argv[1])
+    # IP_parameter = '192.168.1.17/24'
+    # create new instance
+    myNetwork = net.Network(IP_parameter)
+
+    # this method to determine the class type by prefix // not accurate
+    if myNetwork.prefix is not None:
+        print(f"using prefix this is class {myNetwork.class_type()}")
+
+    # this method run if only IP
+    myNetwork.ip_type()
+    print(f"IP designation is {myNetwork.designation_type}")
+    print(f"IP is of Class {myNetwork.class_type}")
+
+
 if __name__ == '__main__':
     pass
