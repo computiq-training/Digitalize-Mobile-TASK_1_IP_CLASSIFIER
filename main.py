008e0f4a2115100e35be3779d4022f137207fbec
Aisha <Aisha.cs548@student.uomosul.edu.iq>
2022-10-13 16:18:01 +0300

    my task

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def solution():
-    pass
+import sys
+
+count = len(sys.argv)
 
 
-if __name__ == '__main__':
-    pass
+
+if sys.argv[1] < '128' :
+    print (' class : A'+ '\n' +'designation : public')
+elif sys.argv[1] < '192' :
+    print (' class : B'+ '\n' +'designation : private')
+elif sys.argv[1] < '224' :
+    print (' class : C'+ '\n' +'designation : private')
+elif sys.argv[1] < '240' :
+    print (' class : D'+ '\n' +'designation : private')
