b72907b8fbf5204a6a673c3bf3ba27912cd3e4c1
hiba707 <105562151+hiba707@users.noreply.github.com>
2022-10-13 16:04:44 +0300

    task solution

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 def solution():
-    pass
+    p_address=sys.argv[1]
+    ip=ip_address.split('.')
+    address=int(ip[0])
+
+    if address in range(0,128):
+        if ip[0]=='10':
+            print("class: A Designation:private")
+        else:
+            print("class: A Designation:public")
+    elif address in range(128,192):
+        if ip[0]=='172' and ip[1] in range(16,32):
+            print("class: B Designation:private")
+        else:
+            print("class: B Designation:public")
+    elif address in range(192,224):
+        if ip[1]=='168':
+            print("class: C Designation:private")
+        else:
+            print("class: c Designation:public")
+    elif address in range(224,240):
+        print("class: D")
+    elif address in range (240,256):
+        print("class: E")
+    else:
+        print("wrong ip address")
+    
 
 
 if __name__ == '__main__':
-    pass
+    solution()
+    
